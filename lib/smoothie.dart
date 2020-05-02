290ab9d30a8c83abed94002b3f69b7b0b18a5005
Alex Kuzmin <alex.kuzmin@protonmail.ch>
2020-05-02 13:03:46 +0800

    Improve naming and documentation

--- lib/smoothie.dart
+++ lib/smoothie.dart
@@ -8,27 +8,29 @@ import 'package:vector_math/vector_math.dart';
 class Smoothie {
   ///Creates a sampled curve going through the [originalDataPoints].
   ///
-  ///Returns a list of [sectionPointsCount] * [originalDataPoints] points that lay on a curve going through the [originalDataPoints].
-  List<Point> getSampledCurve(
-      num sectionPointsCount, List<Point> originalDataPoints) {
+  ///[oversamplingFactor] is a number of points of the resulting smoothed curve per single point of the original curve
+  ///[originalDataPoints] is a list of the original points
+  ///
+  ///Returns a list of [oversamplingFactor] * [originalDataPoints.length] points that lay on a curve going through the [originalDataPoints].
+  List<Point> getSampledCurve(num oversamplingFactor, List<Point> originalDataPoints) {
     var sampledCurve = List<Point>();
     for (int sc = 0; sc < originalDataPoints.length - 1; sc++) {
       var curve = new CubicBezier(
         [
           //start
           Vector2(0, originalDataPoints[sc].y.toDouble()),
-          Vector2(sectionPointsCount.toDouble() / 2,
+          Vector2(oversamplingFactor.toDouble() / 2,
               originalDataPoints[sc].y.toDouble()),
-          Vector2(sectionPointsCount.toDouble() / 2,
+          Vector2(oversamplingFactor.toDouble() / 2,
               originalDataPoints[sc + 1].y.toDouble()),
           //end
-          Vector2(sectionPointsCount.toDouble(),
+          Vector2(oversamplingFactor.toDouble(),
               originalDataPoints[sc + 1].y.toDouble()),
         ],
       );
-      for (int j = 0; j < sectionPointsCount; j++) {
-        sampledCurve.add(Point((sc * sectionPointsCount + j).toDouble(),
-            curve.pointAt((j * 1 / sectionPointsCount).toDouble())[1]));
+      for (int j = 0; j < oversamplingFactor; j++) {
+        sampledCurve.add(Point((sc * oversamplingFactor + j).toDouble(),
+            curve.pointAt((j * 1 / oversamplingFactor).toDouble())[1]));
       }
     }
     return sampledCurve;
